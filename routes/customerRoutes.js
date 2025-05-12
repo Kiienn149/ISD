697e7668155d893bb29f8eea373f46108e0f881a
tahailong03 <tahailong03@gmail.com>
2025-05-12 22:51:33 +0700

    13/5

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -16,3 +16,31 @@ router.get('/customer/edit/:id', isManager, editCustomer);
 router.post('/customer/update/:id', isManager, updateCustomer);
 
 module.exports = router;
+
+// Route AJAX: render nội dung khách hàng (không layout)
+router.get('/customer/content', isManager, async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    let totalPurchaseAmount = 0;
+    let totalDebt = 0;
+
+    customers.forEach(customer => {
+      totalPurchaseAmount += customer.totalPurchaseAmount || 0;
+      totalDebt += customer.totalDebt || 0;
+    });
+
+    // Trả về HTML cho phần thân khách hàng
+    res.render('customer/partials/customer-content', {
+      customers,
+      totalPurchaseAmount,
+      totalDebt,
+      suppliers: [], // tránh lỗi EJS nếu dùng chung giao diện
+      supplierTotalPurchaseAmount: 0,
+      supplierTotalDebt: 0
+    });
+  } catch (error) {
+    console.error('[Lỗi /customer/content]', error);
+    res.status(500).send('Không thể tải nội dung khách hàng');
+  }
+});
+
